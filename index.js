b65b7fe7bb6d1496793dabdb6047c7332d339246
Sara Hopper <sara.hopper@gmail.com>
2017-08-29 17:32:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+function bumpCounter(counter) {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+
+}
+
+
+function  createAnimal(animalType) {
+  return function(deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
