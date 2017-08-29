f635f4538b13e7be2bdf3d057ad92c500e1b3578
Sara Hopper <sara.hopper@gmail.com>
2017-08-29 15:56:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    counter += 1
+  }
+  function getBumps() {
+    console.log counter
+  }
+}
